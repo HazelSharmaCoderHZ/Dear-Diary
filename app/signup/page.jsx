54303dd8123dc1e5efe809e21e7101a2f0f36250
HazelSharmaCoderHZ <sharmahazel310@gmail.com>
2025-05-29 12:59:34 +0530

    done

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -16,15 +16,15 @@ export default function SignupPage() {
       await createUserWithEmailAndPassword(auth, email, password);
       router.push("/dashboard");
     } catch (error) {
-      alert("Signup failed: " + error.message);
+      alert("Signup failed: already existing user");
     }
   };
 
   return (
    <div className= "animatedbg">
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className="bg-white/10 shd bg-opacity-50 backdrop-blur-md p-11 rounded-2xl  shadow-md text-center max-w-ls w-full">
-        <h1 className="text-6xl font-bold mb-3 text-cyan-100">Create Your Account</h1>
+      <div className="bg-white/10 bg-opacity-50 shd backdrop-blur-md px-6 py-8 sm:px-10 sm:py-11 rounded-2xl shadow-md text-center w-[90%] max-w-md">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 text-cyan-100">Create Your Account</h1>
         <form onSubmit={handleSignup}>
           <input
             type="email"
@@ -44,7 +44,8 @@ export default function SignupPage() {
           />
           <button
             type="submit"
-            className="bg-purple-500 text-white py-2 px-4 rounded-xl hover:bg-purple-300 transition w-50 border border-purple/30"
+            className="bg-purple-500 text-white py-2 px-6 rounded-xl hover:bg-purple-300 transition w-full sm:w-48 mx-auto"
+
           >
             Sign Up
           </button>
